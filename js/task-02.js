24dc737be59541e6d1458f623a010419ce58370c
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-09-03 15:58:24 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const listEl = document.querySelector('#ingredients');
+console.log(listEl);
+
+for (const ingredient of ingredients) {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = ingredient;
+
+  listEl.append(itemEl);
+}
+
+
+
