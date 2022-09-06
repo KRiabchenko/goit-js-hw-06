9e12a1eb8bcda95af2e31e06f00a736528929c24
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-09-06 15:40:53 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector('#name-input');
+console.log(inputEl);
+
+const spanEl = document.querySelector('#name-output');
+console.log(spanEl);
+
+inputEl.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    spanEl.textContent = event.currentTarget.value;
+     if (event.currentTarget.value === '') {
+    spanEl.textContent = 'Anonymous'
+}
+}
