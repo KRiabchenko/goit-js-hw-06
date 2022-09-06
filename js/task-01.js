8603785cddf9bdb4aa5ce45a91232aa96ea0ee52
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-09-06 09:51:25 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -2,26 +2,39 @@ const itemEl = document.querySelectorAll('.item');
 console.log('Number of categories:', itemEl.length);
 
 
-const firstTitleEl = itemEl[0].firstElementChild.textContent;
-console.log('Category: ', firstTitleEl)
+itemEl.forEach(titel => {
+    // console.log('Category: ', titel.firstElementChild.textContent)
+    const titelEl = titel.querySelector('h2');
+    console.log('Category: ', titelEl.textContent)
 
-const firstList = itemEl[0].children[1];
+    const itemsList = titel.querySelectorAll('li');
+    console.log('Elements: ', itemsList.length)
 
-const firstEl = firstList.children.length;
-console.log('Elements: ', firstEl)
+})
 
-const secondTitleEl = itemEl[1].firstElementChild.textContent;
-console.log('Category: ', secondTitleEl)
 
-const secondList = itemEl[1].children[1];
+ 
 
-const secondEl = secondList.children.length;
-console.log('Elements: ', secondEl)
+// const firstTitleEl = itemEl[0].firstElementChild.textContent;
+// console.log('Category: ', firstTitleEl)
 
-const thirdTitleEl = itemEl[2].firstElementChild.textContent;
-console.log('Category: ', thirdTitleEl)
+// const firstList = itemEl[0].children[1];
 
-const thirdList = itemEl[2].children[1];
+// const firstEl = firstList.children.length;
+// console.log('Elements: ', firstEl)
 
-const thirdEl = thirdList.children.length;
-console.log('Elements: ', thirdEl)
+// const secondTitleEl = itemEl[1].firstElementChild.textContent;
+// console.log('Category: ', secondTitleEl)
+
+// const secondList = itemEl[1].children[1];
+
+// const secondEl = secondList.children.length;
+// console.log('Elements: ', secondEl)
+
+// const thirdTitleEl = itemEl[2].firstElementChild.textContent;
+// console.log('Category: ', thirdTitleEl)
+
+// const thirdList = itemEl[2].children[1];
+
+// const thirdEl = thirdList.children.length;
+// console.log('Elements: ', thirdEl)
