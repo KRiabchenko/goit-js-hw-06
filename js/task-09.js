07c4debeb8f4fed4615787bda284db50a39c6a87
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-09-07 13:59:21 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const btnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+btnEl.addEventListener('click', onBtnChangeColor)
+
+function onBtnChangeColor() {
+spanEl.style.backgroundColor = getRandomHexColor();
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
