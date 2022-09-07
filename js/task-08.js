11c64627e40ae83c766cfde51d1cb3e058b4e860
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-09-07 12:44:11 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+    
+
+    const formData = { mail, password }
+    
+    if (mail === '' || password === '') {
+        return window.alert("Все поля должны быть заполнены!");
+    }
+
+    console.log(`Login: ${mail}, Password: ${password}`);
+    event.currentTarget.reset();
+}
